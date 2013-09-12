5de583b5faa3c5ce27ce4c520fa43c73ee5e4380
Jeremy Sweet <Jeremys@fullsail.edu>
2013-09-12 06:29:59 -0400

    Fixed mismatched key pair spelling.

--- js/json.js
+++ js/json.js
@@ -2,37 +2,37 @@ var json = {
 	"item1": {
 		"name": ["Name:", "Eggs"],
 		"cost": ["Cost:", "3"],
-		"ammount": ["Ammount:", "10"],
+		"amount": ["Amount:", "10"],
 		"description": ["Description:", "Eggs"],
 	},
 	"item2": {
 		"name": ["Name:", "TShirt"],
 		"cost": ["Cost:", "7"],
-		"ammount": ["Ammount:", "19"],
+		"amount": ["Amount:", "19"],
 		"description": ["Description:", "Summer shirts"],
 	},
 	"item3": {
 		"name": ["Name:", "Milk"],
 		"cost": ["Cost:", "2"],
-		"ammount": ["Ammount:", "14"],
+		"amount": ["Amount:", "14"],
 		"description": ["Description:", "Chicken came first!"],
 	},
 	"item4": {
 		"name": ["Name:", "Cheese"],
 		"cost": ["Cost:", "2"],
-		"ammount": ["Ammount:", "7"],
+		"amount": ["Amount:", "7"],
 		"description": ["Description:", "Yummy"],
 	},
 	"item5": {
 		"name": ["Name:", "Coco Milk"],
 		"cost": ["Cost:", "2"],
-		"ammount": ["Ammount:", "4"],
+		"amount": ["Amount:", "4"],
 		"description": ["Description:", "Chicken came first!"],
 	},
 	"item6": {
 		"name": ["Name:", "Window Cleaner"],
 		"cost": ["Cost:", "4"],
-		"ammount": ["Ammount:", "2"],
+		"amount": ["Amount:", "2"],
 		"description": ["Description:", "Window Cleaner"],
 	}
 }
